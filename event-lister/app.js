c4f4145b6a7db9230e09cb90204cdb14f5b8c4ee
DiLeonté Burnside <dileontedburnside@gmail.com>
2022-07-14 14:27:21 -0700

    starting

---
+++ event-lister/app.js
@@ -0,0 +1,30 @@
+function buttonRed() { document.getElementById("color").style.backgroundColor = "red" }
+
+document.getElementById("color").addEventListener("mousedown", buttonRed)
+
+function buttonGreen() { document.getElementById("color").style.backgroundColor = "green" }
+
+document.getElementById("color").addEventListener("dblclick", buttonGreen)
+
+function buttonBlue() { document.getElementById("color").style.backgroundColor = "blue" }
+
+document.getElementById("color").addEventListener("mouseover", buttonBlue)
+
+function buttonOrange() { document.getElementById("color").style.backgroundColor = "orange" }
+
+document.body.addEventListener("wheel", buttonOrange)
+
+function buttonYellow() { document.getElementById("color").style.backgroundColor = "yellow" }
+
+document.getElementById("color").addEventListener("mouseup", buttonYellow)
+
+
+
+
+
+
+
+
+
+
+
