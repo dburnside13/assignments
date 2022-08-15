16ded3358a0f19ace000944210386ccb23dc944f
DiLeonté Burnside <dileontedburnside@gmail.com>
2022-08-14 22:15:05 -0700

    practice

---
+++ node/loop-olympics/main.js
@@ -0,0 +1,15 @@
+const readline = require("readline-sync")
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for (var i = 0; i < numbers.length; i++){
+        console.log(i)
+    }
+
+    for (var i = numbers.length - 1; i >= 0; i--){
+        console.log(numbers[i])
+    }
+
+const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+    for (var i = 0; i < fruit.length; i++){
+        console.log(fruit[i])
+    }
